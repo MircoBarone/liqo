ee5dd6c9c7eccee2c3b28acc6ded0c9ebe8c8e3c
Mirco Barone <mirco.barone@polito.it>
2026-05-24 17:12:45 +0000

    fix(wireguard): integrate upstream userspace implementation in multi-tunnel setup

--- pkg/gateway/tunnel/wireguard/netlink.go
+++ pkg/gateway/tunnel/wireguard/netlink.go
@@ -124,25 +124,66 @@ func runWgUserCmd(cmd *exec.Cmd) {
 	}
 }
 
-// createLinkUserspace creates a new Wireguard interface using the userspace implementation (wireguard-go).
-// TODO: at the moment is not possible to override the settings of the wireguard-go implementation.
-// We are planning a PR to add a flag for the MTU.
-func createLinkUserspace(ctx context.Context, _ *Options, idx int) error {
-	name := tunnel.GetTunnelName(idx)
-	cmd := exec.Command("/usr/bin/wireguard-go", "-f", name) //nolint:gosec //we leave it as it is
-	go runWgUserCmd(cmd)
-
-	if err := wait.PollUntilContextTimeout(ctx, time.Second, 10*time.Second, true, func(context.Context) (done bool, err error) {
-		klog.Infof("Waiting for wireguard device %q to be created", name)
-		if _, err = netlink.LinkByName(name); err != nil {
-			klog.Errorf("failed to get wireguard device '%s': %s", name, err)
-			return false, nil
-		}
-		return true, nil
-	}); err != nil {
-		return fmt.Errorf("failed to create wireguard device %q: %w", name, err)
+// createLinkUserspace creates a new Wireguard interface using the userspace implementation (wireguard-go)
+// embedded as a library. The device is kept running in-process for the lifetime of the gateway.
+func createLinkUserspace(ctx context.Context, options *Options, idx int) error {
+	mtu := options.MTU
+	if mtu <= 0 {
+		mtu = device.DefaultMTU
 	}
 
+	tunDev, err := tun.CreateTUN(tunnel.GetTunnelName(idx), mtu)
+	if err != nil {
+		return fmt.Errorf("failed to create wireguard TUN device %q: %w", tunnel.GetTunnelName(idx), err)
+	}
+
+	name, err := tunDev.Name()
+	if err != nil {
+		_ = tunDev.Close()
+		return fmt.Errorf("failed to read wireguard TUN device name: %w", err)
+	}
+
+	fileUAPI, err := ipc.UAPIOpen(name)
+	if err != nil {
+		_ = tunDev.Close()
+		return fmt.Errorf("failed to open UAPI socket for %q: %w", name, err)
+	}
+
+	logger := device.NewLogger(device.LogLevelError, fmt.Sprintf("(%s) ", name))
+	wgDev := device.NewDevice(tunDev, conn.NewDefaultBind(), logger)
+
+	uapi, err := ipc.UAPIListen(name, fileUAPI)
+	if err != nil {
+		wgDev.Close()
+		return fmt.Errorf("failed to listen on UAPI socket for %q: %w", name, err)
+	}
+
+	go func() {
+		for {
+			c, err := uapi.Accept()
+			if err != nil {
+				return
+			}
+			go wgDev.IpcHandle(c)
+		}
+	}()
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			if err := uapi.Close(); err != nil {
+				klog.Errorf("Error closing uapi: %v", err)
+			}
+			wgDev.Close()
+		case <-wgDev.Wait():
+			if err := uapi.Close(); err != nil {
+				klog.Errorf("Error closing uapi: %v", err)
+			}
+			klog.Fatalf("wireguard userspace device %q stopped unexpectedly", name)
+		}
+	}()
+
+	klog.Infof("wireguard userspace device %q started", name)
 	return nil
 }
 
